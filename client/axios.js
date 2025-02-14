bbe48feb27d90da0373a2a4c2a981e506eed9e8f
natnael0024 <nathanleg23@gmail.com>
2025-02-14 10:35:03 +0300

    backend url update

--- client/axios.js
+++ client/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8002/api',
+    // baseURL: 'http://localhost:8002/api',
+    baseURL: 'https://mystoreapi-3p5y.onrender.com/api',
     // withCredentials: true, 
   });
 
